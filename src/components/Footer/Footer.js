1157fbf1913031100236c314a95195f52a250ba9
Prodev2017 <chris.bergman320@gmail.com>
2017-07-07 07:44:30 -0500

    add login

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,5 +1,9 @@
 import React from 'react';
 
+import poweredByLogo  from './images/powered-by-skuchain.png';
+
+import './styles/Footer.css'
+
 
 const baseCssClassName = 'footer';
 
@@ -7,7 +11,9 @@ const baseCssClassName = 'footer';
 function Footer () {
 	return (
 		<div className={baseCssClassName}>
-			footer
+			<p>
+				<img src={poweredByLogo} width={80} alt={'powered by'} />
+			</p>
 		</div>
 	);
 }
--- src/components/app/App.js
+++ src/components/app/App.js
@@ -3,13 +3,34 @@ import React, { Component } from 'react';
 import MainLayout from '../../layouts/Main';
 
 import Login from '../../components/Login';
+import Main from '../../components/Main';
 
 
 class App extends Component {
+	constructor(props, context) {
+		super(props, context);
+
+		this.state = {
+			isAuthenticated: false,
+		};
+	}
+	_handleLoginSuccess = () => {
+		this.setState({
+			isAuthenticated: true,
+		});
+
+	}
+
 	render () {
+		const {
+			isAuthenticated,
+		} = this.state;
+
 		return (
 			<MainLayout>
-				<Login />
+				{ !isAuthenticated && (<Login onSuccess={this._handleLoginSuccess} />) }
+
+				{ isAuthenticated && (<Main />)}
 
 			</MainLayout>
 		);
